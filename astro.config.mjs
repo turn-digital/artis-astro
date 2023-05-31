878eb53003b6189e02c00c1c234ceb37f08439a7
Vladyslav Tkachenko <vlad@wtf.lv>
2023-05-31 19:57:04 +0300

    adding new files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://turn-digital.github.io",
+  site: "https://turn-digital.github.io/",
   // base: "/artis-astro",
   integrations: [
     astroI18next(),
